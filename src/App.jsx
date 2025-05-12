304dc188bd4cfd8b64bc1d9b02cf67247a21b36b
Ali Maher <gotothebower@gmail.com>
2025-05-12 14:17:32 +0300

    add localization , and translat for Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,51 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/** @format */
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Partners from './pages/Partners';
-import Contact from './pages/Contact';
-import Menu from './pages/Menu';
-import Cart from './pages/Cart';
-import { CartProvider } from './context/CartContext';
-import { ThemeProvider } from './context/ThemeContext';
-
-
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Partners from "./pages/Partners";
+import Contact from "./pages/Contact";
+import Menu from "./pages/Menu";
+import Cart from "./pages/Cart";
+import { CartProvider } from "./context/CartContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return (
-    <Router>
-      <ThemeProvider>
-        <CartProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/partners" element={<Partners />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/cart" element={<Cart />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </CartProvider>
-      </ThemeProvider>
-    </Router>
-  );
+	const { i18n } = useTranslation();
+
+	document.body.dir = i18n.language === "ar" ? "rtl" : "ltr";
+
+	const changeLanguage = (lng) => {
+		i18n.changeLanguage(lng);
+	};
+
+	return (
+		<Router>
+			<ThemeProvider>
+				<CartProvider>
+					<div className="min-h-screen flex flex-col">
+						<Navbar changeLanguage={changeLanguage} />
+						<main className="flex-grow">
+							<Routes>
+								<Route path="/" element={<Home />} />
+								<Route path="/about" element={<About />} />
+								<Route path="/partners" element={<Partners />} />
+								<Route path="/contact" element={<Contact />} />
+								<Route path="/menu" element={<Menu />} />
+								<Route path="/cart" element={<Cart />} />
+							</Routes>
+						</main>
+						<Footer />
+					</div>
+				</CartProvider>
+			</ThemeProvider>
+		</Router>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/** @format */
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { I18nextProvider } from "react-i18next";
+import i18next from "./i18n"; 
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<I18nextProvider i18n={i18next}>
+			<App />
+		</I18nextProvider>
+	</StrictMode>
+);
