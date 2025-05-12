412cfca4000f15a5445bcb30e0ce19f35d88f997
Ahmed Ali <113573377+ahmedali221@users.noreply.github.com>
2025-05-12 15:10:04 +0300

    merge branch 'Ali' of github.com:Ali-Dev/React-Redux-Toolkit into Ali

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Partners from './pages/Partners';
-import Contact from './pages/Contact';
-import Menu from './pages/Menu';
-import Cart from './pages/Cart';
-import Payment from './pages/Payment'; // Add this import
-import { CartProvider } from './context/CartContext';
-import { ThemeProvider } from './context/ThemeContext';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -23,35 +12,14 @@ import Partners from "./pages/Partners";
 import Contact from "./pages/Contact";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
+import Payment from "./pages/Payment";
 import { CartProvider } from "./context/CartContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  return (
-    <Router>
-      <ThemeProvider>
-        <CartProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navbar />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/partners" element={<Partners />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/payment" element={<Payment />} /> {/* Add this route */}
-              </Routes>
-            </main>
-            <Footer />
-          </div>
-        </CartProvider>
-      </ThemeProvider>
-    </Router>
-  );
   const { i18n } = useTranslation();
 
+  // Set the direction of the document based on the selected language
   document.body.dir = i18n.language === "ar" ? "rtl" : "ltr";
 
   const changeLanguage = (lng) => {
@@ -62,20 +30,20 @@ function App() {
     <Router>
       <ThemeProvider>
         <CartProvider>
-          <div className="min-h-screen flex flex-col">
-            <Navbar changeLanguage={changeLanguage} />
-            <main className="flex-grow">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/partners" element={<Partners />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/menu" element={<Menu />} />
-                <Route path="/cart" element={<Cart />} />
-              </Routes>
-            </main>
-            <Footer />
-          </div>
+          <div className="min-h-screen flex flex-col"></div>
+          <Navbar changeLanguage={changeLanguage} />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/partners" element={<Partners />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/payment" element={<Payment />} />
+            </Routes>
+          </main>
+          <Footer />
         </CartProvider>
       </ThemeProvider>
     </Router>
