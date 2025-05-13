38056410b288ec3ae0218e60f85f9670ba8651ec
Ahmed Ali <113573377+ahmedali221@users.noreply.github.com>
2025-05-13 17:27:40 +0300

    updated layouts& animations

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,11 +7,6 @@ export default function HeroSection({ setSearchQuery, image }) {
 	const [localSearchQuery, setLocalSearchQuery] = useState("");
 	const { t, i18n } = useTranslation();
 
-	const handleSearchChange = (e) => {
-		setLocalSearchQuery(e.target.value);
-		setSearchQuery(e.target.value);
-		console.log("HeroSection:", e.target.value);
-	};
 
 	return (
 		<div
@@ -22,18 +17,7 @@ export default function HeroSection({ setSearchQuery, image }) {
 				backgroundColor: "rgba(0, 0, 0, 0.5)",
 			}}
 			dir={i18n.language === "ar" ? "rtl" : "ltr"}>
-			<div className="text-center px-4 sm:px-6 lg:px-8 w-full max-w-3xl">
-				<h1 className="text-5xl font-extrabold mb-6 drop-shadow-lg">
-					{t("hero_section.title")}
-				</h1>
-				<input
-					type="text"
-					placeholder={t("hero_section.search_placeholder")}
-					className="input input-bordered w-full text-black bg-white"
-					value={localSearchQuery}
-					onChange={handleSearchChange}
-				/>
-			</div>
+			
 		</div>
 	);
 }
